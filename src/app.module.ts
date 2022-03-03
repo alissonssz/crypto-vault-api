f3074eb221ac19b13b47015df606087e551056a4
Alisson <alissonssz@hotmail.com>
2022-03-03 00:02:06 -0300

    feat(ethereum): add ConfigModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EthereumModule } from './ethereum/ethereum.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [EthereumModule],
+  imports: [EthereumModule, ConfigModule.forRoot({
+    isGlobal: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
